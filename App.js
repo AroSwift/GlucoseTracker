53c8e555d45c1593feee996a9beca5171e2ebc0f
Aaron Barlow <abarlow505@gmail.com>
2019-10-03 16:07:44 -0400

    Implement basic mobile design with reactnativepaper

--- App.js
+++ App.js
@@ -1,137 +1,36 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import { createStackNavigator, createAppContainer } from 'react-navigation';
-import Home from './src/screens/Home';
-
-
-const AppNavigator = createStackNavigator(
-  {
-    Home,
-    AddItem,
-    List
-  },
-  {
-    initialRouteName: 'Home'
-  }
-);
-
-const AppContainer = createAppContainer(AppNavigator);
-
-export default class App extends Component {
-  render() {
-    return <AppContainer />;
-  }
-}
-
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import { Appbar, Provider as PaperProvider, Text } from 'react-native-paper';
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
+  bottom: {
     position: 'absolute',
+    left: 0,
     right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+    bottom: 0,
+  }
+})
+
+class App extends Component {
+  render() {
+    return (
+      <PaperProvider>
+        <Appbar.Header>
+          <Appbar.Content
+            title="GlucoseTracker"
+            subtitle="Home"
+          />
+        </Appbar.Header>
+
+        <Appbar style={styles.bottom}>
+          <Appbar.Action icon="archive" onPress={() => console.log('Pressed archive')} />
+          <Appbar.Action icon="mail" onPress={() => console.log('Pressed mail')} />
+          <Appbar.Action icon="label" onPress={() => console.log('Pressed label')} />
+          <Appbar.Action icon="delete" onPress={() => console.log('Pressed delete')} />
+        </Appbar>
+      </PaperProvider>
+      )
+  }
+}
 
 export default App;
