a47b2ab5b1854618cc904d5a105329095f0eddb6
Aaron Barlow <abarlow505@gmail.com>
2019-10-26 13:33:54 -0400

    Basic web design complete

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,36 @@
 import * as React from 'react';
-import { Button } from 'react-native-paper';
-import './App.css';
+import { View } from 'react-native';
+import { MdAccessibility, MdAccessible, MdSettings } from "react-icons/md"; // how to use: https://react-icons.netlify.com/#/icons/fa
+import {
+  Button, Text, Appbar, Provider as PaperProvider
+} from 'react-native-paper';
+import './stylesheets/Main.css';
+// import './App.css';
 
-function App() {
-  return (
-    <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
-      Press me
-    </Button>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <PaperProvider>
+        <Appbar.Header>
+          <Appbar.Content
+            title="Glucose Tracker"
+          />
+          <Button mode="text" onPress={() => console.log('glucose')} color="white">
+            <MdAccessible className="icon"/>
+            Patients
+          </Button>
+
+          <Button mode="text" onPress={() => console.log('nutrition')} color="white">
+            <MdAccessibility className="icon"/>
+            Doctors
+          </Button>
+
+          <Button mode="text" onPress={() => console.log('Settings')} color="white">
+            <MdSettings className="icon"/>
+            Settings
+          </Button>
+        </Appbar.Header>
+      </PaperProvider>
+    );
+  }
 }
-
-export default App;
