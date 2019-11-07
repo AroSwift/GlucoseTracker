c38e4f7e1c1d7a7f83eac4aefa1a14275a5a0242
cathelynj <55094022+cathelynj@users.noreply.github.com>
2019-11-07 14:25:20 -0500

    Update SignUpScreen.tsx
    Added functionality that allows users to sign up.

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,14 +1,15 @@
 // Get all the necessary components from React
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, AsyncStorage } from 'react-native';
 import {
   Button, TextInput, Text, Surface, Card, Provider as PaperProvider
 } from 'react-native-paper';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 // Get all the stylesheets
 import { styles } from '../stylesheets/Main';
 //import db from 'firebase';
 import { firebase } from '../config';
-
 
 export default class SignUpScreen extends React.Component {
 
@@ -17,24 +18,74 @@ export default class SignUpScreen extends React.Component {
     this.state = {
           email: '',
           password: '',
+          user_uid: null,
+          auth_id: null,
           errorMessage: null
-    };
+        };
+    }
 
 
-  }
-
-
-  async handleSignUp(email, pass) {
+  async handleSignUp() {
 
       try {
           await firebase.auth()
-              .createUserWithEmailAndPassword(email, pass);
+              .createUserWithEmailAndPassword(this.state.email, this.state.password)
+              .then(data => {
+                this.setState({ auth_id: data.user.uid});
+              });
+
+          // Create UID and save to Firebase
+          let users = firebase.firestore().collection('users').doc()
+            .set({
+                  admin: false,
+                  auth_id: this.state.auth_id,
+                  first_name: '',
+                  last_name: ''
+                })
+
 
           console.log("Account created");
+          console.log(this.state.auth_id);
 
-          // Navigate to the Home page, the user is auto logged in
+          //Set user uid
+          let query = await users.where('auth_id', '==', this.state.auth_id).get()
+            .then(snapshot => {
+              var user_uids = []
+              snapshot.forEach(doc => {
+                user_uids.push(doc.id);
+              });
+              console.log(user_uids);
+              this.setState({ user_uid: user_uids[0] });
+            })
 
-      } catch (error) {
+          await AsyncStorage.setItem('@GlucoseTracker:user_uid', this.state.user_uid);
+          await AsyncStorage.setItem('@GlucoseTracker:auth_id', this.state.auth_id);
+          await AsyncStorage.setItem('@GlucoseTracker:email', this.state.email);
+          await AsyncStorage.setItem('@GlucoseTracker:password', this.state.password);
+
+          console.log("Logged In!", this.state.user_uid);
+
+          try {
+            var user_uid = await AsyncStorage.getItem('@GlucoseTracker:user_uid');
+            var auth_id = await AsyncStorage.getItem('@GlucoseTracker:auth_id');
+            var email = await AsyncStorage.getItem('@GlucoseTracker:email');
+            var password = await AsyncStorage.getItem('@GlucoseTracker:password');
+
+            if(auth_id != null && email != null && password != null && user_uid != null) {
+              await firebase.auth()
+                .signInWithEmailAndPassword(email, password);
+
+              console.log("Already logged in", user_uid);
+
+              // Navigate to the Home page
+              return this.props.navigation.replace('MainTemplate', { user_uid: user_uid });
+            }
+          } catch(error) {
+            console.log("Not already logged in");
+          }
+
+      }
+      catch (error) {
           console.log(error.toString())
       }
 
@@ -69,7 +120,7 @@ render() {
           <Button
             title="Login"
             mode="contained"
-            onPress={this.handleSignUp}
+            onPress={() => this.handleSignUp()}
             style={styles.breakAfter}>
             Sign Up
           </Button>
