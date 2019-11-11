c18eb173c6c6b4bc68bcb8ca6497032e3bac81ea
Aaron Barlow <abarlow505@gmail.com>
2019-11-11 01:38:07 -0500

    Get doctor sign in working

--- web/src/screens/SignUpScreen.js
+++ web/src/screens/SignUpScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-// import { AsyncStorage } from 'react-native';
+import { AsyncStorage } from 'react-native';
 import {
   Button, TextInput, Text, Surface, Provider as PaperProvider
 } from 'react-native-paper';
@@ -22,14 +22,43 @@ export default class SignUpScreen extends React.Component {
     };
   }
 
-  async handleSignUp(email, pass) {
-
+  async handleSignUp() {
       try {
-          await firebase.auth()
-              .createUserWithEmailAndPassword(email, pass);
+        await firebase.auth()
+          .createUserWithEmailAndPassword(this.state.email, this.state.password)
+          .then(data => {
+            this.setState({ auth_id: data.user.uid});
+          });
+
+        if(this.state.auth_id != null) {
+          // Create UID and save to Firebase
+          firebase.firestore().collection('doctors').doc()
+            .set({
+              auth_id: this.state.auth_id,
+              first_name: this.state.first_name,
+              last_name: this.state.last_name
+            });
 
           console.log("Account created");
-          // Navigate to the Home page, the user is auto logged in
+          console.log(this.state.auth_id);
+
+          // Persist login now
+          await AsyncStorage.setItem('@GlucoseTracker:user_uid', this.state.user_uid);
+          await AsyncStorage.setItem('@GlucoseTracker:auth_id', this.state.auth_id);
+          await AsyncStorage.setItem('@GlucoseTracker:email', this.state.email);
+          await AsyncStorage.setItem('@GlucoseTracker:password', this.state.password);
+
+          console.log("Logged In!", this.state.user_uid);
+
+          await firebase.auth()
+            .signInWithEmailAndPassword(this.state.email, this.state.password);
+
+          // Login for realz now
+          this.props.on_handle_logged_in(true);
+          this.props.on_handle_user_uid(this.state.user_uid);
+          this.props.on_handle_auth_uid(this.state.auth_uid);
+          this.props.on_handle_user_type(this.state.user_type);
+        }
       } catch (error) {
           console.log(error.toString())
       }
@@ -79,7 +108,7 @@ render() {
           <Button
             title="Login"
             mode="contained"
-            onPress={this.handleSignUp}>
+            onPress={() => this.handleSignUp()}>
             Sign Up
           </Button>
           <div className="smallBreakAfter"></div>
